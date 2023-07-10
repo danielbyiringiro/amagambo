5f3d0de860c6fb6b4c719eb01050deb75bd084f3
danielbyiringiro <daniel.byiringiro@ashesi.edu.gh>
2023-07-10 13:34:00 +0000

    Feat Register Function

--- app.py
+++ app.py
@@ -33,7 +33,8 @@ def after_request(response):
 @login_required
 def index():
     """Show portfolio of stocks"""
-    return None
+    flash("Registered")
+    return "Murakaza neza"
 
 @app.route("/history")
 @login_required
@@ -53,19 +54,22 @@ def login():
     if request.method == "POST":
 
         # Ensure username was submitted
-        if not request.form.get("username"):
-            return None
+        if not request.form.get("email"):
+            flash("Empty email")
+            return redirect("/login")
 
         # Ensure password was submitted
         elif not request.form.get("password"):
-            return None
+            flash("Empty password")
+            return redirect("/login")
 
         # Query database for username
-        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+        rows = db.execute("SELECT * FROM users WHERE email = ?", request.form.get("email"))
 
         # Ensure username exists and password is correct
         if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            return None
+            flash("Username or password is wrong")
+            return redirect("/login")
 
         # Remember which user has logged in
         session["user_id"] = rows[0]["id"]
@@ -91,7 +95,71 @@ def logout():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """Register user"""
-    return None
+    
+    if request.method == "POST":
+        
+        # get email
+        email = request.form.get("email")
+        if not email: #if email is empty
+            flash("Email not provided")
+            return redirect("/register")
+        
+        # check if email already exists
+        exists = search(email)
+        
+        if not exists:
+            flash("Email already exists")
+            return redirect("/register")
+        
+        # get password and the password confirmation
+        password = request.form.get("password")
+        password_confirm = request.form.get("password_confirm")
+
+        # meets security standards
+        response = validate(password)
+        if  response == True:
+            if password != password_confirm:
+                flash("Passwords do not match")
+                return redirect("/")
+            
+            hash = generate_password_hash(password)
+            db.execute("INSERT INTO users(email,hash,password) VALUES(?,?,?)", email,hash,password)
+            rows = db.execute("SELECT id FROM users where email = ?", email)
+            id = rows[0]["id"]
+            session["user_id"] = id
+            
+            # redirect to home page
+            return redirect("/")
+        
+        else:
+            # flash response from validate password
+            flash(response)
+            return redirect("/register")
+           
+    else:
+        return render_template("register.html")
 
 
+def search(email):
+    """Searches if current email is in database"""
+    rows = db.execute("SELECT * FROM users WHERE email = ?", email)
+    #returns a boolean
+    return len(rows) == 0
+    
+def validate(password):
+    """Validates if password meets security policy"""
 
+    #check length
+    if len(password) < 8:
+        return "Password has to be at least 8 characters"
+    
+    #check if password contains a digit
+    if not any([x for x in password if x.isdigit()]):
+        return "Password has to contain at least a single digit"
+    
+    #check for letters:
+    if not any([x for x in password if x.islower() or x.isupper()]):
+        return "Password has to contain at least a single letter"
+    
+    #passed all checks
+    return True
